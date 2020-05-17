4ac701972ec6e4eb97ea008148d27b08b0168be7
DalerAsrorov <asrorids@gmail.com>
2020-05-17 16:56:48 -0400

    Added enableLabels to bar API

--- src/typings/charts/bar-chart.ts
+++ src/typings/charts/bar-chart.ts
@@ -1,32 +1,33 @@
-import { Selection } from 'd3-selection';
-import { ChartBaseAPI } from './base';
+import { Selection } from 'd3-selection'
+import { ChartBaseAPI } from './base'
 
 enum BarChartKeys {
   Value = 'value',
-  Name = 'name',
+  Name = 'name'
 }
 
 export type BarChartDataShape = {
-  [BarChartKeys.Value]: number;
-  [BarChartKeys.Name]: string;
-};
+  [BarChartKeys.Value]: number
+  [BarChartKeys.Name]: string
+}
 
 type BarChartModuleSelection = (
   _selection: Selection<Element, BarChartDataShape[], Element | null, any>,
   _data: BarChartDataShape[]
-) => void;
+) => void
 
 export interface BarChartAPI extends ChartBaseAPI<BarChartModule> {
-  betweenBarsPadding(padding?: number): BarChartModule;
-  chartGradient(gradient?: [string, string]): BarChartModule;
-  hasPercentage(hasPercentage?: boolean): BarChartModule;
-  isHorizontal(isHorizontal?: boolean): BarChartModule;
-  labelsNumberFormat(format?: string): BarChartModule;
-  labelsSize(size?: number): BarChartModule;
-  percentageAxisToMaxRatio(ratio?: number): BarChartModule;
+  betweenBarsPadding(padding?: number): BarChartModule
+  chartGradient(gradient?: [string, string]): BarChartModule
+  enableLabels(shouldEnable?: boolean): BarChartModule
+  hasPercentage(hasPercentage?: boolean): BarChartModule
+  isHorizontal(isHorizontal?: boolean): BarChartModule
+  labelsNumberFormat(format?: string): BarChartModule
+  labelsSize(size?: number): BarChartModule
+  percentageAxisToMaxRatio(ratio?: number): BarChartModule
   orderingFunction(
     orderingFunc: (a: BarChartDataShape, b: BarChartDataShape) => void
-  ): BarChartModule;
+  ): BarChartModule
 }
 
-export type BarChartModule = BarChartModuleSelection & BarChartAPI;
+export type BarChartModule = BarChartModuleSelection & BarChartAPI
