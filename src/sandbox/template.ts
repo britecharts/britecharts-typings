caf7b59a1866ad797f87e4aa404de9578a762ba8
DalerAsrorov <asrorids@gmail.com>
2020-05-17 05:54:00 -0400

    Template built

--- src/sandbox/template.ts
+++ src/sandbox/template.ts
@@ -1,6 +1,8 @@
 export const buildTemplate = (containerId: string): string => `
 <div id="app">
-<h1>Hello Message</h1>
-<div id="${containerId}"></div>
+    <h1>Britecharts TypeScript Sandbox</h1>
+    <div class="app-container">
+        <section id="${containerId}"></section>
+    </div>
 </div>
 `;
