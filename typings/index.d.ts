f2122fd6da5fe4ef24235fadad086dced12551ba
DalerAsrorov <asrorids@gmail.com>
2020-05-17 03:28:03 -0400

    Edit gitignire

---
+++ typings/index.d.ts
@@ -0,0 +1,14 @@
+declare module 'britecharts' {
+  export interface ChartModule<T> {
+    width(width?: number): T & ChartModule<T>;
+    height(height?: number): T & ChartModule<T>;
+    isAnimated(isAnimated?: boolean): T & ChartModule<T>;
+  }
+
+  export interface BarChartModule extends ChartModule<BarChartModule> {
+    betweenBarsPadding(padding?: number): BarChartModule;
+    labelsNumberFormat(format?: string): BarChartModule;
+  }
+
+  function bar(): BarChartModule;
+}
