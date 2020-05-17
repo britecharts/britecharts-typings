5129e0c5707c4411f58f7dd474d5b55d729aa397
DalerAsrorov <asrorids@gmail.com>
2020-05-17 03:22:23 -0400

    More complex typing style added, prettierrc file

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
-import * as britecharts from "britecharts";
-import { buildTemplate } from "./template";
+import * as britecharts from 'britecharts';
+import { buildTemplate } from './template';
 
 export default (window: Window) => {
-  const chartContainerId = "chartArea";
+  const chartContainerId = 'chartArea';
 
   window.document.body.innerHTML = buildTemplate(chartContainerId);
 
   const bar = britecharts.bar();
 
-  console.log(bar.height(100).width(300));
+  bar
+    .width(400)
+    .height(400)
+    .betweenBarsPadding(300)
+    .isAnimated(true)
+    .labelsNumberFormat('.0%');
 };
