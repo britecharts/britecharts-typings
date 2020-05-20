fa57d6b457540c1dd36d4363dd736377d65aa852
DalerAsrorov <asrorids@gmail.com>
2020-05-20 01:51:29 -0400

    allow the user to decide if it's an array type

--- src/typings/charts/bar-chart.d.ts
+++ src/typings/charts/bar-chart.d.ts
@@ -1,35 +1,35 @@
-import { ChartBaseAPI } from '@charts/base';
-import { ChartModuleSelection } from '@charts/selection';
+import { ChartBaseAPI } from '@charts/base'
+import { ChartModuleSelection } from '@charts/selection'
 
 export enum BarChartKeys {
   Value = 'value',
-  Name = 'name',
+  Name = 'name'
 }
 
 export type BarChartDataShape = {
-  [BarChartKeys.Value]: number;
-  [BarChartKeys.Name]: string;
-};
-
-export interface BarChartAPI extends ChartBaseAPI<BarChartModule> {
-  betweenBarsPadding(padding?: number): BarChartModule;
-  chartGradient(gradient?: [string, string]): BarChartModule;
-  enableLabels(shouldEnable?: boolean): BarChartModule;
-  hasPercentage(hasPercentage?: boolean): BarChartModule;
-  isHorizontal(isHorizontal?: boolean): BarChartModule;
-  labelsNumberFormat(format?: string): BarChartModule;
-  labelsSize(size?: number): BarChartModule;
-  percentageAxisToMaxRatio(ratio?: number): BarChartModule;
-  orderingFunction(
-    orderingFunc: (a: BarChartDataShape, b: BarChartDataShape) => void
-  ): BarChartModule;
+  [BarChartKeys.Value]: number
+  [BarChartKeys.Name]: string
 }
 
-export type BarChartModule = ChartModuleSelection<BarChartDataShape> &
-  BarChartAPI;
+export interface BarChartAPI extends ChartBaseAPI<BarChartModule> {
+  betweenBarsPadding(padding?: number): BarChartModule
+  chartGradient(gradient?: [string, string]): BarChartModule
+  enableLabels(shouldEnable?: boolean): BarChartModule
+  hasPercentage(hasPercentage?: boolean): BarChartModule
+  isHorizontal(isHorizontal?: boolean): BarChartModule
+  labelsNumberFormat(format?: string): BarChartModule
+  labelsSize(size?: number): BarChartModule
+  percentageAxisToMaxRatio(ratio?: number): BarChartModule
+  orderingFunction(
+    orderingFunc: (a: BarChartDataShape, b: BarChartDataShape) => void
+  ): BarChartModule
+}
+
+export type BarChartModule = ChartModuleSelection<BarChartDataShape[]> &
+  BarChartAPI
 
 /**
  * import {bar} from 'britecharts;
  * bar().width(100).height(100)
  */
-export function bar(): BarChartModule;
+export function bar (): BarChartModule
--- src/typings/charts/selection.d.ts
+++ src/typings/charts/selection.d.ts
@@ -1,6 +1,6 @@
-import { Selection } from 'd3-selection';
+import { Selection } from 'd3-selection'
 
 export type ChartModuleSelection<T> = (
-  _selection: Selection<Element, T[], Element | null, any>,
-  _data: T[]
-) => void;
+  _selection: Selection<Element, T, Element | null, any>,
+  _data: T
+) => void
