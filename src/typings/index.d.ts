c245232d9e55266e124fd1815e46d505f180b8bc
DalerAsrorov <asrorids@gmail.com>
2020-05-21 00:31:19 -0400

    Bullet chart typings and demo

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,5 +1,6 @@
 declare module 'britecharts' {
   export * from '@charts/bar-chart'
   export * from '@charts/donut-chart'
+  export * from '@charts/bullet-chart'
   export * from '@helpers/colors'
 }
