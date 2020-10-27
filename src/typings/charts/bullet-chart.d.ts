fce3c639e2c37e0dcf30d9526eedad9dd6e03c6c
Marcos Iglesias <190833+Golodhros@users.noreply.github.com>
2020-10-26 22:15:15 -0700

    Adding documentation to signatures (#27)

--- src/typings/charts/bullet-chart.d.ts
+++ src/typings/charts/bullet-chart.d.ts
@@ -19,12 +19,22 @@ export type BulletChartBaseAPI = Omit<
 >;
 
 export interface BulletChartAPI extends BulletChartBaseAPI, ExportableChartAPI<BulletChartModule> {
+  /** Gets or Sets the aspect ratio of the chart */
   aspectRatio(ratio?: number): BulletChartModule;
+  /** Gets or Sets the subtitle for measure identifier range. */
   customSubtitle(subtitle?: number): BulletChartModule;
+  /** Gets or Sets the title for measure identifier */
   customTitle(title?: number): BulletChartModule;
+  /**
+   * Gets or Sets the isReverse status of the chart. If true,
+   * the elements will be rendered in reverse order.
+   */
   isReverse(isReverse?: boolean): BulletChartModule;
+  /** Space between axis and chart */
   paddingBetweenAxisAndChart(padding?: number): BulletChartModule;
+  /** Gets or Sets the starting point of the capacity range. */
   startMaxRangeOpacity(opacity?: number): BulletChartModule;
+  /** Gets or Sets the number of ticks of the x axis on the chart */
   ticks(ticks?: number): BulletChartModule;
 }
 
